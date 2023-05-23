f6019cf487c9842bea22958b1171a170faa2a28f
Trisha <121923741+Trishanomer@users.noreply.github.com>
2023-05-23 09:56:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def is_admins(chat_id: int):
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("Hi! My name is Ishi. I'm an Artificial Intelligence\n /chatbot - [on|off]")
+    await message.reply_text("Hi! My name is ␌亗 ＧＯＫＵ. I'm an Artificial Intelligence\n /chatbot - [on|off]")
 
 # Add other command handlers here
 
